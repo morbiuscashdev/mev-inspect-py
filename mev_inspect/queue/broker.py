d22986d920b6fea302f32db4cc75d2be0a08e400
morbiuscashdev <242494938+morbiuscashdev@users.noreply.github.com>
2026-02-18 22:38:31 -0600

    feat(queue): make Redis host and port configurable via env vars

--- mev_inspect/queue/broker.py
+++ mev_inspect/queue/broker.py
@@ -4,4 +4,8 @@ from dramatiq.brokers.redis import RedisBroker
 
 
 def connect_broker():
-    return RedisBroker(host="redis-master", password=os.environ["REDIS_PASSWORD"])
+    return RedisBroker(
+        host=os.environ.get("REDIS_HOST", "redis-master"),
+        port=int(os.environ.get("REDIS_PORT", "6379")),
+        password=os.environ["REDIS_PASSWORD"],
+    )
